eab94956ea4827db0b932c8899ab060fffd84866
galax <galaxw@gmail.com>
2017-08-31 11:03:04 -0700

    Setup parse_request accounting for randomized data and integer output

--- solution.php
+++ solution.php
@@ -1,9 +1,48 @@
 <?php
 // YOUR NAME AND EMAIL GOES HERE
+//Galax Womack galaxw@gmail.com
 
 function parse_request($request, $secret)
 {
     // YOUR CODE GOES HERE
+	//DECODE REQUEST AND VALIDATE SIGNATURE
+
+	$out = array();
+
+	$init = explode('.', $request);
+	$decoded_sig = base64_decode($init[0]);
+	$decoded = base64_decode($init[1]);
+	$json_decoded = json_decode($decoded, true);
+	$json_size = sizeof($json_decoded);
+
+	//SETUP OUTPUT ARRAY
+	if ($json_size === 3) {
+
+		foreach ($json_decoded as $key => $value) {
+
+			$out[':' . $key] = $value;
+
+		}
+
+		$date_test = date_parse($out[':date']);
+
+		if ($out[':user_id'] && $out[':score'] && $out[':date'] && $date_test && !$date_test['errors']) {
+			return (check_signature($decoded, $decoded_sig, $secret)) ? $out : false;
+		} else {
+			return false;
+		}
+
+	} else {
+		return (is_int($json_decoded)) ? $json_decoded : false;
+	}
+
+}
+
+function check_signature($payload, $signature, $secret=API_SECRET) {
+	//CONFIRM SIGNATURE
+	$out = (hash_hmac('sha256', $payload, $secret, true) == $signature) ? true : false;
+
+	return $out;
 }
 
 function total_number_of_valid_requests($pdo)
